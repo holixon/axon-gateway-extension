1d106f7f0b548d289c442c3763d15f524ed39891
Simon Zambrovski <simon.zambrovski@holisticon.de>
2022-11-09 21:36:08 +0100

    improve codacy

--- extension/spring-boot-autoconfigure/src/test/kotlin/io/holixon/axon/gateway/configuration/command/ManualPredicateConfiguration.kt
+++ extension/spring-boot-autoconfigure/src/test/kotlin/io/holixon/axon/gateway/configuration/command/ManualPredicateConfiguration.kt
@@ -5,9 +5,15 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import java.util.function.Predicate
 
+/**
+ * Example of manual config.
+ */
 class ManualPredicateConfiguration {
 
 
+  /**
+   * For example a predicate on a name.
+   */
   @Bean
   @Qualifier(COMMAND_DISPATCH)
   fun otherCommandPredicate(): Predicate<String> = Predicate<String> { name -> name == "foo.Name" }
